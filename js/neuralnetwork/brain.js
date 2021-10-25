44fa6a7953365de680c37cd4aa9470a3d7e4ee05
Bryan Cook <bryandeancook@gmail.com>
2021-10-25 09:10:37 -0600

    Code cleanup Removed some old test code that didn't work out.

--- js/neuralnetwork/brain.js
+++ js/neuralnetwork/brain.js
@@ -45,30 +45,4 @@ class Brain {
     }
     return inputs;
   };
-  save() {
-    let layers = [];
-    for (let j = 0; j < this.layers.length; j++) {
-      let layer = [];
-      for (let k = 0; k < this.layers[j].neurons.length; k++) {
-        let weights = [];
-        for (let l = 0; l < this.layers[j].neurons[k].weights.length; l++) {
-          weights.push(this.layers[j].neurons[k].weights[l]);
-        }
-        layer.push([weights, this.layers[j].neurons[k].bias]);
-      }
-      layers.push(layer);
-    }
-    return JSON.stringify(layers);
-  };
-  load(brainData) {
-    poo
-    for (let j = 0; j < brainData.brains[i].length; j++) {
-      for (let k = 0; k < brainData.brains[i][j].length; k++) {
-        for (let l = 0; l < brainData.brains[i][j][k][0].length; l++) {
-          this.brains[i].layers[j].neurons[k][0][l] = brainData.brains[i][j][k][0][l];
-        }
-        this.brains[i].layers[j].neurons[k].bias = brainData.brains[i][j][k][1];
-      }
-    }
-  };
 };
